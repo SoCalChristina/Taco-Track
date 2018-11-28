17b1af0ae167c19aaf13d1cd793926b0ca329802
Christina-Tovar-Hamernik <ctovarhamernik@gmail.com>
2018-11-27 16:31:49 -0800

    initial branch commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# Taco Track
+
+## Udacity Frontend Developer Nano Degree Project 7
+
+## Summary:
+Taco Track is Taco Truck mapping app. Taco Track uses API data to map, locate, and display information on Taco Trucks in Oxnard, California. A marker identifies the location of each taco truck on the map. The user clicks on the designated map marker to receive additional information about the Taco Truck, including a photograph when available. It's worth taking note that none of the local taco trucks have websites but utilize Facebook as an alternative.
+
+The user clicks on the hamburger menu icon to access a search input window and a pre-populated list of Taco Trucks. Users can track and enjoy some of the best Mexican food in the country.
+
+![screen shot of app](https://github.com/SoCalChristina/react-neighborhood-map/blob/master/src/images/app-image.png "Taco Track")
+
+## Purpose:
+This is a Reactjs Neighborhood Map app built as the final project of the Udacity Frontend Developer Nano Degree.
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Getting Started:
+### `Clone or fork repository.`
+(https://github.com/SoCalChristina/react-neighborhood-map)
+
+### `npm install`
+
+Adds the required dependencies.
+
+### `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br>
+You will also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.<br>
+
+### Service Worker
+This app uses the default service worker provided in the starter code. The service worker only works in production build.
+
+The service worker is opt-in only. To opt-in, open the serviceWorker.js file and change registration.unregister(); to registration.register();.
+
+If you need to test your offline-first service worker locally, build the application (using npm run build) and run a simple http server from your build directory.
+See: (https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app#offline-first-considerations) for more information.
+
+## Attributions
+### This project utilizes the following:
+* Google Maps
+(https://cloud.google.com/maps-platform/)
+* Google Maps React (https://github.com/fullstackreact/google-maps-react)
+* @material-ui/core (https://www.npmjs.com/package/@material-ui/core)
+* Font Awesome
+(https://fontawesome.com/)
+* FourSquare Developers Places API data (https://developer.foursquare.com/places-api),
+
+* Taco Truck icon made by Freepik (https://www.freepik.com/) from Flaticon (https://www.flaticon.com/) and licensed by (http://creativecommons.org/licenses/by/3.0/) Creative Commons BY 3.0
+
+
+## References
+All resources are referenced here and/or in the code.
+* FEND P7 Walkthrough with Doug Brown
+(https://www.youtube.com/watch?reload=9&v=NVAVLCJwAAo&feature=youtu.be)
+* Doug Brown's fend-maps-walkthrough
+(https://github.com/thefinitemonkey/fend-maps-walkthrough/blob/master/src/components/MapDisplay.js)
+* Coding Session - Google Maps With React JS
+(https://www.youtube.com/watch?v=5J6fs_BlVC0&feature=youtu.be)
+* Neighborhood Map (Series of 6 Videos) by Forrest Walker
+(https://www.youtube.com/playlist?list=PL4rQq4MQP1crXuPtruu_eijgOUUXhcUCP)
+* Facebook Create React App
+(https://facebook.github.io/create-react-app/docs/getting-started)
+* [React documentation](https://reactjs.org/).
---
+++ src/App.js
@@ -0,0 +1,106 @@
+import React, {Component} from 'react';
+
+import './App.css';
+//add an image to react. Source: https://stackoverflow.com/questions/37644265/correct-path-for-img-on-react-js
+import logo from './images/taco-truck.png';
+import Locations from './data/Locations.json';
+import MapDisplay from './components/MapDisplay';
+import ListDrawer from './components/ListDrawer';
+
+class App extends Component {
+    state = {
+      //set neighborhood with latitude and longitude values
+        lat: 34.1975048,
+        lon: -119.1770516,
+        all: Locations,
+        //enlarge map display
+        zoom: 12,
+        open: false
+    }
+
+    styles = {
+        menuButton: {
+            marginLeft: 10,
+            marginRight: 20,
+            position: "absolute",
+            left: 10,
+            top: 40,
+            background: "#fff",
+            padding: 5
+        },
+        hide: {
+            display: 'none'
+        },
+        header: {
+            marginTop: "0px"
+        }
+      };
+      //https://developmentarc.gitbooks.io/react-indepth/content/life_cycle/birth/post_mount_with_component_did_mount.html
+      componentDidMount = () => {
+        //https://facebook.github.io/react-native/docs/state.html
+        this.setState({
+          ...this.state,
+          filtered: this.filterLocations(this.state.all, "")
+        });
+      }
+
+      toggleDrawer = () => {
+        // Toggle the value controlling whether the drawer is displayed//
+          this.setState({
+              open: !this.state.open
+          });
+      }
+
+      updateQuery = (query) => {
+        // filter business location info return by query input
+          this.setState({
+              ...this.state,
+              selectedIndex: null,
+              filtered: this.filterLocations(this.state.all, query)
+          });
+      }
+
+      filterLocations = (Locations, query) => {
+          // Filter locations to match query string
+          return Locations.filter(location => location.name.toLowerCase().includes(query.toLowerCase()));
+      }
+
+      clickListItem = (index) => {
+          // Set the state to reflect the selected location array index
+          this.setState({ selectedIndex: index, open: !this.state.open })
+      }
+
+      render = () => {
+          return (
+              <div className="App">
+                  {/*TODO create and install components for menu button, header, map display, and footer.*/}
+                  <div className="app-header">
+                      <button onClick={this.toggleDrawer} style={this.styles.menuButton}>
+                          <i className="fa fa-bars"></i>
+                      </button>
+                      <h1>Oxnard Taco Track</h1>
+                      <img src={logo} className="app-logo" alt={"taco truck logo"} />
+
+                  </div>
+
+                  <MapDisplay
+                      lat={this.state.lat}
+                      lon={this.state.lon}
+                      zoom={this.state.zoom}
+                      Locations={this.state.filtered}
+                      selectedIndex={this.state.selectedIndex}
+                      clickListItem={this.clickListItem}/>
+                  <ListDrawer
+                      Locations={this.state.filtered}
+                      open={this.state.open}
+                      toggleDrawer={this.toggleDrawer}
+                      filterLocations={this.updateQuery}
+                      clickListItem={this.clickListItem}/>
+                  </div>
+                  //TODO add footer with copyright and contact info
+              );
+          }
+      }
+
+
+export default App;
